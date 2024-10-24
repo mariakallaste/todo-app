10bb6c9a100b334e165da597825415196edbbea7
mariakallaste <144336453+mariakallaste@users.noreply.github.com>
2024-10-24 10:01:38 +0300

    first commit

---
+++ controllers/todo.controller.js
@@ -0,0 +1,47 @@
+import { PrismaClient, TodoStatus } from "@prisma/client";
+
+const prisma = new PrismaClient()
+
+export const getAllTodos = async (request, response) => {
+
+    try {
+
+        const todos = await prisma.todo.findMany()
+
+        response.status(200).json({
+            todos
+        })
+
+    } catch (error) {
+        console.log(error)
+        response.status(500).json({
+            message: "Bad luck. Try agian."
+        })
+    }
+}
+
+export const createTodo = async (request, response) => {
+    const { title, description } = request.body
+
+    try {
+
+        const todo = await prisma.todo.create({
+            data: {
+                title,
+                description,
+                status: TodoStatus.TODO
+            }
+        })
+
+        response.status(201).json({
+            message: "Todo created successfully.",
+            todo
+        })
+
+    } catch (error) {
+        console.log(error)
+        response.status(500).json ({
+            message: "Bad luck. Try again!"
+        })
+    }
+}
--- server.js
+++ server.js
@@ -2,24 +2,17 @@
 
 import express from "express";
 import dotenv from "dotenv"  //salastatud väärtuste pärast
+import todoRouter from "./routes/todo.router.js";
 
 dotenv.config()  //get saame browserist kätte, POST ei saa
 
 const PORT = process.env.PORT || 3006 //lambi port ja PORT läheb app.listen ka aga käsk et võta .env PORT ja kui seal ei ole siis siit.
 
-const animals = ["Koer", "Lammas", "Jõehobu", "Elevant"]
-
 const app = express() //teen mooduli ja käivitan expressi seal sees
 
-app.get("/", (request, response) => {
-    response.status(200).json({
-        message: "Reede on. Mine koju!"
-    })
-})  //see on root kaust, esimene koht kuhu klient maandub (kaldkriips seda näitab)
+app.use(express.json())
 
-app.get("/animals", (request, response) => {
-  response.status(200).json(animals);
-});
+app.use('/api/v1', todoRouter)
 
 app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`); //
